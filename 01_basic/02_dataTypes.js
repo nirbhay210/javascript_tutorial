c2b4e2721e5fbc47e5db072468f56664ae92579b
nirbhay210 <143868239+nirbhay210@users.noreply.github.com>
2023-09-22 10:03:03 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // Treat all js code as a newer version
+
+// alert(3+3) // we are using nodeJs not browser
+
+/* console.log( 3
+    + 3)  dont do this code readability should be high */
+
+// console.log("nirbhay");
+
+let name = "nirbhay" 
+let age = 35
+let isLoggedIn = false 
+
+// number>= 2 to power 53
+// bigint
+// string=>""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof name)
+console.log(typeof age);
+console.log(typeof isLoggedIn)
+// console.table([typeof name, age, isLoggedIn])
+// console.log(typeof [name, age, isLoggedIn])
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
